e4170be9a6567bd7bb770269df1c0a1388eca477
vbungay <113390913+vbungay@users.noreply.github.com>
2022-11-27 18:29:37 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import AlertModule from 'ngx-bootstrap';
+
 
 //MAT TOOLBARS
 import { MatToolbarModule } from '@angular/material/toolbar';
